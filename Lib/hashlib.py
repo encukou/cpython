cbd8eaec53fcdc97c5d9db66369bd49df135396d
Petr Viktorin <pviktori@redhat.com>
2019-07-04 17:23:46 +0200

    Use hash algorithms strictly from _hashopenssl
    Define several new hashes (sha3-*, shake*, blake*), using a new
    hack: a header file listing all the hashes is included multiple
    times.
    
    Use only algorithms from _hashopenssl; don't import _md5 & friends.
    
    This means blake2 loses its special functionality.

--- Lib/hashlib.py
+++ Lib/hashlib.py
@@ -77,32 +77,26 @@ def __get_builtin_constructor(name):
         return constructor
     try:
         if name in ('SHA1', 'sha1'):
-            import _sha1
-            cache['SHA1'] = cache['sha1'] = _sha1.sha1
+            cache['SHA1'] = cache['sha1'] = _hashlib.openssl_sha1
         elif name in ('MD5', 'md5'):
-            import _md5
-            cache['MD5'] = cache['md5'] = _md5.md5
+            cache['MD5'] = cache['md5'] = _hashlib.openssl_md5
         elif name in ('SHA256', 'sha256', 'SHA224', 'sha224'):
-            import _sha256
-            cache['SHA224'] = cache['sha224'] = _sha256.sha224
-            cache['SHA256'] = cache['sha256'] = _sha256.sha256
+            cache['SHA224'] = cache['sha224'] = _hashlib.openssl_sha224
+            cache['SHA256'] = cache['sha256'] = _hashlib.openssl_sha256
         elif name in ('SHA512', 'sha512', 'SHA384', 'sha384'):
-            import _sha512
-            cache['SHA384'] = cache['sha384'] = _sha512.sha384
-            cache['SHA512'] = cache['sha512'] = _sha512.sha512
+            cache['SHA384'] = cache['sha384'] = _hashlib.openssl_sha384
+            cache['SHA512'] = cache['sha512'] = _hashlib.openssl_sha512
         elif name in ('blake2b', 'blake2s'):
-            import _blake2
-            cache['blake2b'] = _blake2.blake2b
-            cache['blake2s'] = _blake2.blake2s
+            cache['blake2b'] = _hashlib.openssl_blake2b
+            cache['blake2s'] = _hashlib.openssl_blake2s
         elif name in {'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512',
                       'shake_128', 'shake_256'}:
-            import _sha3
-            cache['sha3_224'] = _sha3.sha3_224
-            cache['sha3_256'] = _sha3.sha3_256
-            cache['sha3_384'] = _sha3.sha3_384
-            cache['sha3_512'] = _sha3.sha3_512
-            cache['shake_128'] = _sha3.shake_128
-            cache['shake_256'] = _sha3.shake_256
+            cache['sha3_224'] = _hashlib.openssl_sha3_224
+            cache['sha3_256'] = _hashlib.openssl_sha3_256
+            cache['sha3_384'] = _hashlib.openssl_sha3_384
+            cache['sha3_512'] = _hashlib.openssl_sha3_512
+            cache['shake_128'] = _hashlib.openssl_shake_128
+            cache['shake_256'] = _hashlib.openssl_shake_256
     except ImportError:
         pass  # no extension module, this hash is unsupported.
 
