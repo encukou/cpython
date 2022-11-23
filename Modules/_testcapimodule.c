42e60cddd9efd10db0788e2937d1eb71fcd406c1
Petr Viktorin <encukou@gmail.com>
2022-11-23 16:30:37 +0100

    Test code extra storage

--- Modules/_testcapimodule.c
+++ Modules/_testcapimodule.c
@@ -4054,6 +4054,9 @@ PyInit__testcapi(void)
     if (_PyTestCapi_Init_Structmember(m) < 0) {
         return NULL;
     }
+    if (_PyTestCapi_Init_Code(m) < 0) {
+        return NULL;
+    }
 
 #ifndef LIMITED_API_AVAILABLE
     PyModule_AddObjectRef(m, "LIMITED_API_AVAILABLE", Py_False);
