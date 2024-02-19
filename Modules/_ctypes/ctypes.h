009f6fbf79171cea371a89ed75699f23045be7b4
Petr Viktorin <encukou@gmail.com>
2024-02-19 21:46:01 +0100

    Move `length` from StgDict to StgInfo, except for sizeof calculation

--- Modules/_ctypes/ctypes.h
+++ Modules/_ctypes/ctypes.h
@@ -232,6 +232,7 @@ typedef struct {
     int initialized;
     Py_ssize_t size;            /* number of bytes */
     Py_ssize_t align;           /* alignment requirements */
+    Py_ssize_t length;          /* number of fields */
 
     PARAMFUNC paramfunc;
 } StgInfo;
@@ -261,7 +262,7 @@ typedef struct {
 */
     //Py_ssize_t size;            /* number of bytes */
     //Py_ssize_t align;           /* alignment requirements */
-    Py_ssize_t length;          /* number of fields */
+    //Py_ssize_t length;          /* number of fields */
     ffi_type ffi_type_pointer;
     PyObject *proto;            /* Only for Pointer/ArrayObject */
     SETFUNC setfunc;            /* Only for simple objects */
