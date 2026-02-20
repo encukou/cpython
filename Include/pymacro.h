686c929577ef70b0c030e8ab3170d0a3ac7da691
Petr Viktorin <encukou@gmail.com>
2026-02-20 12:25:32 +0100

    Smaller Py_ARRAY_LENGTH

--- Include/pymacro.h
+++ Include/pymacro.h
@@ -182,15 +182,18 @@
 
    Written by Rusty Russell, public domain, http://ccodearchive.net/
 
-   Requires at GCC 3.1+ */
-#if (defined(__GNUC__) && !defined(__STRICT_ANSI__) && \
-    (((__GNUC__ == 3) && (__GNUC_MINOR__ >= 1)) || (__GNUC__ >= 4)))
-/* Two gcc extensions.
-   &a[0] degrades to a pointer: a different type from an array */
-#define Py_ARRAY_LENGTH(array) \
-    (sizeof(array) / sizeof((array)[0]) \
-     + Py_BUILD_ASSERT_EXPR(!__builtin_types_compatible_p(typeof(array), \
-                                                          typeof(&(array)[0]))))
+    */
+#if defined(__GNUC__) && !defined(__STRICT_ANSI__) && (0 + __GNUC__ > 4)
+/* Two GCC extensions: builtin & statement expression. Tested on GCC 5.1. */
+/* &a[0] degrades to a pointer: a different type from an array */
+#define Py_ARRAY_LENGTH(array)                                  \
+    ({                                                          \
+        static_assert(!__builtin_types_compatible_p(            \
+            typeof(array),                                      \
+            typeof(&(array)[0])));                              \
+        (sizeof(array) / sizeof((array)[0]))                    \
+    })                                                          \
+    /////////////////////////////////////////////////////////////
 #else
 #define Py_ARRAY_LENGTH(array) \
     (sizeof(array) / sizeof((array)[0]))
